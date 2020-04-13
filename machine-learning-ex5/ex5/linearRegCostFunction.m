ba81e551ca8601d08adf2a7b90235285d7eb5367
abdulkk49 <kadirkhimani5@gmail.com>
2020-04-13 22:01:42 +0530

    initial commit

---
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -0,0 +1,42 @@
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear 
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+h = X*theta - y;
+J1 = 1/(2*m) * (h' *h);
+J2 = (lambda/(2*m))* ((theta(2:end, :))' * theta(2:end, :));
+J = J1 + J2;
+
+grad1 = (1/m) * (X' * h);
+grad2 = (lambda/m) * [0; theta(2:end, 1)];
+grad = grad1 + grad2;
+
+
+
+
+
+
+
+
+
+% =========================================================================
+
+grad = grad(:);
+
+end
---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+C_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10];
+sigma_vec = [0 0.001 0.003 0.01 0.03 0.1 0.3 1 3 10];
+
+% You need to return these variables correctly.
+
+
+
+%Training 64 models 
+for i = 1:length(sigma_vec)
+  sigma = sigma_vec(i);
+  for j = 1:length(C_vec)
+     C = C_vec(j);
+     model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+     predictions = svmPredict(model, Xval);
+     pred_error(i,j) = mean(double(predictions ~= yval));
+  endfor
+endfor
+
+%Returning index having minimum pred_error
+[minval,ind] = min(pred_error(:));
+[I,J] = ind2sub([size(pred_error,1) size(pred_error,2)],ind);
+
+C = C_vec(J);
+sigma = sigma_vec(I);
+
+fprintf('%f %f' , C, sigma);
+% =========================================================================
+
+end
